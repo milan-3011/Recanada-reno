ea2f225e7dba80080aed8b534190412da3c84cd3
Milan Patel <milanrpatel30@gmail.com>
2024-10-21 11:11:57 +0530

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,45 @@
+$(document).ready(function() {
+  $("#header").load("/General/header.html", function() {
+    timelinecodes(); 
+    scrolltricodes(); 
+
+    if ($(window).width() < 1110) {
+      let hamburgers = $(".hamburger");
+
+      hamburgers.each(function() {
+        let tl = gsap.timeline({ paused: true });
+
+        tl.to(".navbar-links", {
+          right: 0,
+          duration: 0.6,
+          ease: "power2.inOut"
+        })
+        .from(".navbar-links li", {
+          x: 150,
+          duration: 0.3,
+          stagger: 0.3,
+          opacity: 0,
+        });
+
+        $(this).on("click", function() {
+          $(this).toggleClass("openmenu");
+          if ($(this).hasClass("openmenu")) {
+            tl.play();  
+          } else {
+            tl.reverse();
+          }
+        });
+      });
+    }
+
+    $("#footer").load("/General/footer.html");
+  });
+
+  scrollwithLoco();
+});
+
+
+// Function to set up Locomotive Scroll and GSAP ScrollTrigger
 function scrollwithLoco() {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -28,54 +70,50 @@ function scrollwithLoco() {
   ScrollTrigger.refresh();
 }
 
-scrollwithLoco();
-
-
+// Timeline animations function
 function timelinecodes() {
   var tl = gsap.timeline();
 
-  tl.from(".contact-info, #header",{
+  tl.from(".contact-info, #header", {
     y: -30,
     opacity: 0,
     duration: 1,
-  } )
-  tl.from(".sec-line h4", {
+  })
+  .from(".sec-line h4", {
     y: 60,
     opacity: 0,
     duration: 0.5,
-    stagger:0.3,
+    stagger: 0.3,
   })
-  tl.from(".first-img", {
+  .from(".first-img", {
     x: 60,
     opacity: 0,
     duration: 0.5,
   })
-  tl.from(".who-we-are, .who-we-are div", {
+  .from(".who-we-are, .who-we-are div", {
     y: 100, 
     opacity: 0, 
     duration: 1, 
     ease: "ease-in-out", 
     stagger: 0.3,
-
   });
 }
-timelinecodes();
 
-function scrolltricodes(){
+// Scroll-triggered animations function
+function scrolltricodes() {
   const isTab = window.innerWidth < 1110;
-
 
   gsap.to(".logo-name", {
     y: -100, 
     opacity: 0, 
     duration: 1, 
     ease: "ease-in-out", 
-    stagger:0.3,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: ".page1", 
       scroller: "#main",
       start: "top -10%",
-      end:"top 50%",
+      end: "top 50%",
       scrub: 2, 
     }
   });
@@ -87,12 +125,12 @@ function scrolltricodes(){
     opacity: 0, 
     duration: 0.5, 
     ease: "ease-in-out", 
-    stagger:0.3,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: ".page3", 
       scroller: "#main",
       start: srvs,
-      end:"top 50%",
+      end: "top 50%",
       scrub: 5, 
     }
   });
@@ -111,7 +149,7 @@ function scrolltricodes(){
       scrub: 5,
     }
   });
-  
+
   // Animate the bottom 3 images later
   gsap.from(".images div:nth-child(n+4)", { 
     y: 100, 
@@ -133,12 +171,12 @@ function scrolltricodes(){
     opacity: 0, 
     duration: 0.5, 
     ease: "ease-in-out", 
-    stagger:0.3,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: ".page4", 
       scroller: "#main",
       start: "top 50%",
-      end:"top 50%",
+      end: "top 50%",
       scrub: 3, 
     }
   });
@@ -150,28 +188,30 @@ function scrolltricodes(){
     opacity: 0, 
     duration: 0.5, 
     ease: "ease-in-out", 
-    stagger:0.3,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: ".page5", 
       scroller: "#main",
       start: whyus,
-      end:"top 50%",
+      end: "top 50%",
       scrub: 2, 
     }
   });
+
   gsap.from(".que a h3", {
-    scale:1.3,
+    scale: 1.3,
     opacity: 0, 
-    duration:2.5,
+    duration: 2.5,
     ease: "bounce.out",
     scrollTrigger: {
       trigger: ".page6", 
       scroller: "#main",
       start: "top 60%",
-      end:"top 50%",
-      scrub:2,
+      end: "top 50%",
+      scrub: 2,
     }
   });
+
   const queP = isTab ? "top 110%" : "top 60%";
 
   gsap.from(".que p", {
@@ -179,58 +219,38 @@ function scrolltricodes(){
     opacity: 0, 
     duration: 0.5, 
     ease: "ease-in-out", 
-    stagger:0.3,
+    stagger: 0.3,
     scrollTrigger: {
       trigger: ".page6", 
       scroller: "#main",
       start: queP,
-      scrub:2,
+      scrub: 2,
     }
   });
+
   gsap.from(".ftr", {
     y: 100,
     opacity: 0, 
-    duration:0.5,
+    duration: 0.5,
     ease: "ease-in-out",
     scrollTrigger: {
       trigger: ".ftr", 
       scroller: "#main",
       start: "top 100%",
-      end:"top 90%",
-      scrub:2,
+      end: "top 90%",
+      scrub: 2,
     }
   });
- 
-  
 }
-scrolltricodes();
 
-if (window.innerWidth < 1110) {
-  let hamburgers = document.getElementsByClassName("hamburger");
-
-  for (let i = 0; i < hamburgers.length; i++) {
-      let hamburger = hamburgers[i];
-      var tl = gsap.timeline({ paused: true });
-
-      tl.to(".navbar-links", {
-          right: 0,
-          duration: 0.6,
-          ease: "power2.inOut"
-      })
-      .from(".navbar-links li", {
-          x: 150,
-          duration: 0.3,
-          stagger: 0.3,
-          opacity: 0,
-      });
-
-      hamburger.addEventListener("click", function() {
-          this.classList.toggle("openmenu");
-          if (this.classList.contains("openmenu")) {
-              tl.play();  
-          } else {
-              tl.reverse(); 
-          }
-      });
+function urlHandel(){
+  if (window.location.pathname === "/about/") {
+    window.location.href = "/Pages/aboutUs.html";
+  } else if (window.location.pathname === "/services/") {
+    window.location.href = "/Pages/services.html";
+  } else if (window.location.pathname === "/contact/") {
+    window.location.href = "/Pages/contactUs.html";
   }
 }
+urlHandel();
+
