f695b838ffa699776545495c2073b8b2883552aa
Milan Patel <milanrpatel30@gmail.com>
2024-10-23 09:26:09 +0530

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,8 +52,8 @@ function scrollwithLoco() {
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
-    smooth: true,
-    smoothMobile: true
+    smooth: false,
+    smoothMobile: false
   });
 
   locoScroll.on("scroll", ScrollTrigger.update);
